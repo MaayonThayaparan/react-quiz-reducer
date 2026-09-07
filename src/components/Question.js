60a431fb443542fd20088fc2e394a46c77358953
Maayon Thayaparan <maayon.thayaparan@gmail.com>
2026-09-06 20:18:55 -0400

    Refactored to use Context Provider, removed prop drilling

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,13 @@
 import Options from "./Options";
+import { useQuiz } from "../contexts/QuizContext";
 
-function Question({ question, dispatch, answer }) {
+function Question() {
+  const { questions, dispatch, answer, index } = useQuiz();
+  const question = questions[index];
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} dispatch={dispatch} answer={answer} />
+      <Options />
     </div>
   );
 }
